db53d491bfe2f4aeccba621e15257416f212ea1f
goodnesshealthcaredata-cmd <goodnesshealthcaredata@gmail.com>
2026-05-23 23:24:21 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,8 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
-        "./",
-        "./index.html"
+        "/goodnesshealthcare/",
+        "/goodnesshealthcare/login.html"
       ]);
     })
   );
