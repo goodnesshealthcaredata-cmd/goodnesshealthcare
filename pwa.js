8b0f71898a0e8036336278beae224fc674d10270
goodnesshealthcaredata-cmd <goodnesshealthcaredata@gmail.com>
2026-05-23 23:40:23 +0530

    Update pwa.js

--- pwa.js
+++ pwa.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/goodnesshealthcare/service-worker.js", {
-    scope: "/goodnesshealthcare/"
-  })
-    .then(reg => console.log("SW Registered:", reg.scope))
-    .catch(err => console.log("SW failed:", err));
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/goodnesshealthcare/service-worker.js")
+      .then(reg => console.log("SW registered:", reg.scope))
+      .catch(err => console.log("SW error:", err));
+  });
 }
