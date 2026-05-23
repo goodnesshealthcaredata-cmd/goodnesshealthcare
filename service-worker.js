750b8c94bb3a658540cec7e12794130dd5c66042
goodnesshealthcaredata-cmd <goodnesshealthcaredata@gmail.com>
2026-05-23 23:55:02 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,30 @@
-const CACHE_NAME = "goodness-v1";
+const CACHE_NAME = "goodness-cache-v5";
+
+const urlsToCache = [
+  "/goodnesshealthcare/",
+  "/goodnesshealthcare/login.html"
+];
 
 self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    clients.claim()
+  );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
